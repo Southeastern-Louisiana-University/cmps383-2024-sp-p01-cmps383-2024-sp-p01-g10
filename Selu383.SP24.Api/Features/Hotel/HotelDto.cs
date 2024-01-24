6fb2654a5a1b93b4c5a8cb3cdf5f4a6d504439c6
Aone Shrestha <aoneshrestha58@gmail.com>
2024-01-23 23:13:10 -0600

    creating basic entities

---
+++ Selu383.SP24.Api/Features/Hotel/HotelDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Selu383.SP24.Api.Features.Hotel
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
